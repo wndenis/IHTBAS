284f9906ca0bbbc3b9aad634db9e027e3efc1c93
Denis <winda.wzse@gmail.com>
2017-03-05 21:49:42 +0300

    Второй уровень добавлен

--- Assets/Demo_assets/Demo_scripts/CameraRotation.cs
+++ Assets/Demo_assets/Demo_scripts/CameraRotation.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
+    public Transform bg;
+    private Transform tr;
+    public bool rotate = true;
 
 	// Use this for initialization
 	void Start () {
-        
+        tr = GetComponent<Transform>();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, 90*Time.deltaTime);
-	}
+        if (rotate)
+        {
+            tr.Rotate(0, 0, -8 * Time.deltaTime);
+            bg.Rotate(0, 0, -8 * Time.deltaTime);
+        }
+    }
 }
